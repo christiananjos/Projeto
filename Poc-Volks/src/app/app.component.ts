b5180f2c9c47532eda7143d042b549af90f4c102
Christian Anjos <christiananjos@hotmail.com>
2025-03-01 16:30:03 -0300

    implementação do carousel

--- Poc-Volks/src/app/app.component.ts
+++ Poc-Volks/src/app/app.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Poc-Volks'
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 }
--- Poc-Volks/src/app/app.module.ts
+++ Poc-Volks/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCarouselModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 @NgModule({
@@ -9,7 +9,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    NgbCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
