ade51f077b198b1d893bc7c7f146b66c0a23f530
Christian Anjos <christiananjos@hotmail.com>
2025-03-04 20:48:59 -0300

    primeira parte da validação quando tiver saldo em atraso

--- Poc-Volks/src/app/app.component.ts
+++ Poc-Volks/src/app/app.component.ts
@@ -6,14 +6,65 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Poc-Volks'
-
+export default class AppComponent {
   showSaldo = true;
-  showEmAtraso = true;
+  showEmAtraso = false;
+  txtEmAtraso: string = '';
 
   toggleSaldo() {
     this.showSaldo = !this.showSaldo;
   }
+
+  ngOnInit() {
+    this.validateOnLoad();
+  }
+
+  validateOnLoad() {
+    if (this.showEmAtraso == false) {
+      this.txtEmAtraso = "EM DIA";
+      this.changeFontColorToGreen();
+      this.DesabilitaDivMensagemCobranca();
+    } else {
+      this.changeFontColorToRed();
+      this.txtEmAtraso = "EM ATRASO";
+      this.HabilitaDivMensagemCobranca();
+
+     
+    }
+  }
+
+  changeFontColorToRed() {
+    const element = document.getElementById('statusContrato');
+    if (element) {
+      element.style.color = 'red';
+    }
+  }
+
+  changeFontColorToGreen() {
+    const element = document.getElementById('statusContrato');
+    if (element) {
+      element.style.color ='#3CBE00';
+    }
+  }
+
+  DesabilitaDivMensagemCobranca() {
+    const element = document.getElementById('divMensagemCobranca');
+    if (element) {
+      element.style.display = 'none';
+    }
+  }
+
+  HabilitaDivMensagemCobranca() {
+    const element = document.getElementById("divMensagemCobranca");
+    if (element) {
+      element.style.display = 'block';
+    }
+  }
+
+ 
+
+ 
+
+ 
 
 }
--- Poc-Volks/src/app/app.module.ts
+++ Poc-Volks/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { AppComponent } from './app.component';
+import AppComponent from './app.component';
 import { CarouselBaseComponent } from './carousel-base/carousel-base.component';
 import { FooterBaseComponent } from './footer-base/footer-base.component';
 import { HeaderComponent } from './header/header.component';
