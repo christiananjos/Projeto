8840d878c9cda7e879ed64ccdd5dc3328a5eff5f
Christian Anjos <christiananjos@hotmail.com>
2025-03-11 13:07:34 -0300

    implementei o modal de codigo de barrar valido

--- Poc-Volks/src/app/validador-boletos/validador-boletos.component.ts
+++ Poc-Volks/src/app/validador-boletos/validador-boletos.component.ts
@@ -10,8 +10,22 @@ export class ValidadorBoletosComponent {
 
   barcode: string = '';
 
+  valor: string = 'R$ 1.329,90';
+  vencimento: string = '15/12/2024';
+  nomeBeneficiario: string = 'BANCO VOLKSWAGEN S.A';
+  codigoBarraboleto: string = '23792374039043769259401011189501799600000136036';
+
   onBarcodeInput(event: any): void {
     const input = event.target.value.replace(/\D/g, '');
     this.barcode = input;
   }
+
+  copyToClipboard(): void {
+    navigator.clipboard.writeText(this.codigoBarraboleto).then(() => {
+      alert('Código de barras copiado para a área de transferência!');
+    }).catch(err => {
+      console.error('Erro ao copiar o código de barras: ', err);
+    });
+  }
 }
+
