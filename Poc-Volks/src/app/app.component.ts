a7a5430a0778a76970d8c7b136a8126de6c3a996
Christian Anjos <christiananjos@hotmail.com>
2025-03-13 12:51:20 -0300

    implementado parametrização para o footer

--- Poc-Volks/src/app/app.component.ts
+++ Poc-Volks/src/app/app.component.ts
@@ -7,18 +7,20 @@ import { Component, Renderer2 } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export default class AppComponent {
-  constructor(private renderer: Renderer2) {}
+  constructor(private renderer: Renderer2) { }
+
+  flagEmpresa: string = 'volks';
+
+  // ngonInit() {
+  //   alert('ngonInit');
+  //   // 
+
+  // }
 
 
-flagEmpresa: string = 'volks';
-
-
-//1 - Alterar a cor da fonte geral, cor do footer
-//2 - Alterar as imagens do carousel para cada marca
-//3 - Alterar texto submenu Seguros e Serviços > Vw Protege / Porsche Protege / Carbank Protege
- //3.1 Alterar o texto do cabeçalho para cada marca.
- //3.2 Alterar a imagem do cabeçalho para cada marca.
-//4 - Alterar o modal do validador de boletos com o beficiário de acordo com a marca (valido e invalido)
+  ngOnInit() {
+    this.alterarEstiloETexto(this.flagEmpresa);
+  }
 
   alterarEstiloETexto(flagEmpresa: string): void {
     switch (flagEmpresa) {
@@ -37,9 +39,49 @@ flagEmpresa: string = 'volks';
   }
 
 
-  private alterarParaVolks(): void {}
-  private alterarParaPorsche(): void {}
-  private alterarParaCarbank(): void {}
+  private alterarParaVolks(): void {
+
+    this.alteraCorFonte();
+    this.alterarFooter('##095b73');
+    this.alteraImagensCarousel();
+    this.alteraTextoSubmenuProtege();
+    this.alteraTextoBeneficiarioValidorBoletos();
+
+  }
+  private alterarParaPorsche(): void {
+    // this.alteraCorFonte();
+    this.alterarFooter('#403F44');
+    // this.alteraImagensCarousel();
+    // this.alteraTextoSubmenuProtege();
+    // this.alteraTextoBeneficiarioValidorBoletos();
+  }
+  private alterarParaCarbank(): void {
+
+    this.alteraCorFonte();
+    this.alterarFooter('#00984A');
+    this.alteraImagensCarousel();
+    this.alteraTextoSubmenuProtege();
+    this.alteraTextoBeneficiarioValidorBoletos();
+  }
+
+
+  private alteraCorFonte(): void {
+    //1 - Alterar a cor da fonte geral'
+  }
+
+
+  private alteraImagensCarousel(): void {
+    //2 - Alterar as imagens do carousel para cada marca
+  }
+  private alteraTextoSubmenuProtege(): void {
+    //3 - Alterar texto submenu Seguros e Serviços > Vw Protege / Porsche Protege / Carbank Protege
+    //3.1 Alterar o texto do cabeçalho para cada marca.
+    //3.2 Alterar a imagem do cabeçalho para cada marca.
+  }
+
+  private alteraTextoBeneficiarioValidorBoletos(): void {
+    //4 - Alterar o modal do validador de boletos com o beficiário de acordo com a marca (valido e invalido)
+  }
 
 
   private alterarHeader(): void {
@@ -50,19 +92,23 @@ flagEmpresa: string = 'volks';
     }
   }
 
-  private alterarSidebar(): void {
-    const sidebar = document.querySelector('app-sidebar');
-    if (sidebar) {
-      this.renderer.setStyle(sidebar, 'background-color', 'green');
-      this.renderer.setProperty(sidebar, 'innerText', 'Novo Texto do Sidebar');
+
+  private alterarFooter(codigoRGB: string): void {
+    const footer = document.querySelector('app-footer-base');
+    if (footer) {
+      const footerFull = footer.querySelector('.footer-full');
+      if (footerFull) {
+        this.renderer.setStyle(footerFull, 'background-color', codigoRGB);
+      }
     }
   }
 
-  private alterarFooter(): void {
-    const footer = document.querySelector('app-footer-base');
-    if (footer) {
-      this.renderer.setStyle(footer, 'background-color', 'red');
-      this.renderer.setProperty(footer, 'innerText', 'Novo Texto do Footer');
-    }
-  }
+
+  // private alterarSidebar(): void {
+  //   const sidebar = document.querySelector('app-sidebar');
+  //   if (sidebar) {
+  //     this.renderer.setStyle(sidebar, 'background-color', 'green');
+  //     this.renderer.setProperty(sidebar, 'innerText', 'Novo Texto do Sidebar');
+  //   }
+  // }
 }
